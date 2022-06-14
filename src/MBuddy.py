3b4bc83d5eeabd2882fcf9f6d59e96c841cf4805
Benoit Viguier <benoit.viguier.pro+github@gmail.com>
2022-06-14 21:48:41 +0200

    Stage order

--- src/MBuddy.py
+++ src/MBuddy.py
@@ -5,6 +5,31 @@ from MidiSocket import MidiSocket
 import SelectBox
 import State
 
+stageOrder = (
+    1,  # I Wish
+    10, # Love Foolosophy
+    20, # Street Life
+    2,  # Too Young to Die
+    9,  # Midnight
+    15, # 1975
+    16, # Get Into My Groove
+    24, # Georgy Porgy
+    23, # Somebody else's Guy
+    21, # Work to do
+    22, # Never Too Much
+    7,  # Good Times
+    13, # Love Got in the Way
+    4,  # Sledgehammer
+    12, # I feel it commin'
+)
+
+
+def current_stage_index():
+    try:
+        return stageOrder.index(current_song_id+1)
+    except ValueError:
+        return -1
+
 
 def on_mix_button_pressed(sender):
     v = MixView(state=current_state)
@@ -14,16 +39,19 @@ def on_mix_button_pressed(sender):
 
 
 def on_previous_button_pressed(sender):
-    load_song(current_song_id - 1)
+    stage_index = current_stage_index()
+    load_song(current_song_id - 1 if stage_index == -1 else stageOrder[max(0, stage_index - 1)]-1)
 
 
 def on_next_button_pressed(sender):
-    load_song(current_song_id + 1)
+    stage_index = current_stage_index()
+    load_song(current_song_id + 1 if stage_index == -1 else stageOrder[min(len(stageOrder) - 1, stage_index + 1)]-1)
 
 
 def on_title_button_pressed(sender):
     global current_song_id, scores
     load_song(SelectBox.select(scores, current_song_id))
+
 
 @ui.in_background
 def load_song(new_song_id):
