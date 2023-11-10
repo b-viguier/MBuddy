009ae27f03ec2315a1750fe6940b4454f1cedd3b
Benoit Viguier <benoit.viguier.pro+github@gmail.com>
2023-11-10 23:04:37 +0100

    Amp static files server POC

--- web/index.php
+++ web/index.php
@@ -9,14 +9,49 @@ Amp\Loop::run(function () {
         \Amp\Socket\Server::listen("0.0.0.0:8383"),
     ];
 
-    $server = new \Amp\Http\Server\HttpServer(
-        $sockets,
+    $router = new Amp\Http\Server\Router;
+
+    $router->addRoute(
+        'GET',
+        '/',
         new \Amp\Http\Server\RequestHandler\CallableRequestHandler(function (\Amp\Http\Server\Request $request) {
             return new \Amp\Http\Server\Response(\Amp\Http\Status::OK, [
                 "content-type" => "text/plain; charset=utf-8",
             ], "Hello, World!");
         }),
-        new \Psr\Log\NullLogger(),
+    );
+
+    // Asynchronous FileSystem incompatible with PhpWin
+    $fileSystem = new \Amp\File\Filesystem(new \Amp\File\Driver\BlockingDriver());
+
+    // TODO: incompatible with files containing spaces
+    $documentRoot = new \Amp\Http\Server\StaticContent\DocumentRoot(
+        __DIR__.'/',
+        $fileSystem,
+    );
+    $router->addRoute(
+        'GET',
+        '/scores/{name}',
+        $documentRoot,
+    );
+    $router->addRoute(
+        'GET',
+        '/app.html',
+        $documentRoot,
+    );
+
+
+    $server = new \Amp\Http\Server\HttpServer(
+        $sockets,
+        Amp\Http\Server\Middleware\stack($router, new \Amp\Http\Server\Middleware\ExceptionMiddleware()),
+        new class() extends \Psr\Log\AbstractLogger {
+            public function log($level, $message, array $context = []): void
+            {
+                echo $message."\n";
+                !$context ?: var_dump($context);
+            }
+        },
+        (new \Amp\Http\Server\Options)->withDebugMode(),
     );
 
     yield $server->start();
