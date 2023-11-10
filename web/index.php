045689317f80571d15098689707e3c5b65b7416c
Benoit Viguier <benoit.viguier.pro+github@gmail.com>
2023-11-10 14:53:25 +0100

    Amp Http server POC

--- web/index.php
+++ web/index.php
@@ -2,6 +2,32 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
-echo "<h1>Hello World</h1>";
+Amp\Loop::run(function () {
+    $sockets = [
+        \Amp\Socket\Server::listen("0.0.0.0:8383"),
+    ];
+
+    $server = new \Amp\Http\Server\HttpServer(
+        $sockets,
+        new \Amp\Http\Server\RequestHandler\CallableRequestHandler(function (\Amp\Http\Server\Request $request) {
+            return new \Amp\Http\Server\Response(\Amp\Http\Status::OK, [
+                "content-type" => "text/plain; charset=utf-8",
+            ], "Hello, World!");
+        }),
+        new \Psr\Log\NullLogger(),
+    );
+
+    yield $server->start();
+
+    // Stop the server gracefully when SIGINT is received.
+    // This is technically optional, but it is best to call Server::stop()
+    if (\extension_loaded("pcntl")) {
+        Amp\Loop::onSignal(SIGINT, function (string $watcherId) use ($server) {
+            Amp\Loop::cancel($watcherId);
+            yield $server->stop();
+        });
+    }
+
+});
