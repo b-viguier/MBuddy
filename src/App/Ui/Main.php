3e60ced4fe632516195903da147be622025d1446
Benoit Viguier <benoit.viguier.pro+github@gmail.com>
2024-03-16 21:31:35 +0100

    POC new main UI

--- src/App/Ui/Main.php
+++ src/App/Ui/Main.php
@@ -4,75 +4,47 @@ declare(strict_types=1);
 
 namespace Bveing\MBuddy\App\Ui;
 
-use Bveing\MBuddy\Motif;
 use Bveing\MBuddy\Ui\Component;
-use Bveing\MBuddy\Ui\JsEventBus;
+use Bveing\MBuddy\Ui\Template;
 
 class Main implements Component
 {
+    use Component\Trait\AutoId;
+    use Component\Trait\AutoVersion;
+
     public function __construct(
-        private JsEventBus $jsEventBus,
     ) {
-        $this->navBar = new NavBar($this->jsEventBus, new Motif\Preset());
-
-        $this->motifLayout = new KeyboardLayout(
-            $this->jsEventBus,
-            'Motif',
-            new Motif\Part(Motif\Channel::fromMidiByte(0), 'Piano', true),
-            new Motif\Part(Motif\Channel::fromMidiByte(1), 'Piano', false),
-            new Motif\Part(Motif\Channel::fromMidiByte(2), 'Piano', false),
-            new Motif\Part(Motif\Channel::fromMidiByte(3), 'Piano', false),
-        );
-
-        $this->impulseLayout = new KeyboardLayout(
-            $this->jsEventBus,
-            'Impulse',
-            new Motif\Part(Motif\Channel::fromMidiByte(7), 'Piano', true),
-            new Motif\Part(Motif\Channel::fromMidiByte(8), 'Piano', false),
-            new Motif\Part(Motif\Channel::fromMidiByte(9), 'Piano', false),
-            new Motif\Part(Motif\Channel::fromMidiByte(10), 'Piano', false),
-        );
-
-        $this->scoreViewer = new ScoreViewer(
-            $this->jsEventBus,
-            'scores/01-IWish.png',
-        );
-
-        $this->buttonGroup = new ButtonGroup(
-            $this->jsEventBus,
-        );
+        $this->navBar = new NavBar();
     }
 
-    public function render(): string
+    public function template(): Template
     {
-        return <<<HTML
+        return Template::create(
+            <<<HTML
             <div class="container-fluid h-100 px-0 d-flex flex-column">
                 <!-- NavBar -->
-                {$this->navBar->render()}
+                {{ navbar }}
 
                 <div class="row align-items-center no-gutters flex-grow-1">
 
                 <!-- Left panel -->
                 <div class="col-8 pl-3 pr-1">
-                    {$this->motifLayout->render()}
+                    
                     <div class="mt-4"></div> <!-- Spacer -->
-                    {$this->impulseLayout->render()}
+                    
                 </div>
                 
                 <!-- Right panel -->
                 <div class="col pr-3 pl-1">
-                    {$this->scoreViewer->render()}
+                    
                     <div class="mt-4"></div> <!-- Spacer -->
-                    {$this->buttonGroup->render()}
+                    
                 </div>
                 
             </div>
-            HTML;
+            HTML,
+            navbar: $this->navBar
+        );
     }
     private NavBar $navBar;
-    private KeyboardLayout $motifLayout;
-    private KeyboardLayout $impulseLayout;
-
-    private ScoreViewer $scoreViewer;
-    private ButtonGroup $buttonGroup;
 }
--- src/App/Ui/NavBar.php
+++ src/App/Ui/NavBar.php
@@ -4,39 +4,55 @@ declare(strict_types=1);
 
 namespace Bveing\MBuddy\App\Ui;
 
-use Bveing\MBuddy\Motif\Preset;
 use Bveing\MBuddy\Ui\Component;
-use Bveing\MBuddy\Ui\JsEventBus;
+use Bveing\MBuddy\Ui\Style;
+use Bveing\MBuddy\Ui\Template;
 
 class NavBar implements Component
 {
+    use Component\Trait\AutoId;
+    use Component\Trait\AutoVersion;
+
     public function __construct(
-        private JsEventBus $jsEventBus,
-        private Preset $preset,
     ) {
+        $this->previous = Component\Button::create()
+            ->set(
+                color: Style\Color::PRIMARY(),
+                icon: Style\Icon::ARROW_LEFT_SQUARE_FILL(),
+            );
+        $this->next = Component\Button::create()
+            ->set(
+                color: Style\Color::PRIMARY(),
+                icon: Style\Icon::ARROW_RIGHT_SQUARE_FILL(),
+            );
     }
 
-    public function render(): string
+    public function template(): Template
     {
-        return <<<HTML
+        return Template::create(
+            <<<HTML
             <nav class="navbar navbar-light bg-dark">
-                <button type="button" class="btn btn-primary"><i class="bi bi-arrow-left-square-fill"></i></button>
+                {{ previous }}
                 <form class="form-inline w-75">
                     <div class="input-group w-100">
                         <div class="input-group-prepend">
                             <button class="btn btn-primary" type="button">Preset</button>
                         </div>
-                        <input type="text" class="form-control user-select-none" placeholder="{$this->preset->getMasterName()}" readonly>
-                        <input type="text" class="form-control user-select-none" placeholder="{$this->preset->getSongName()}" readonly>
+                        <input type="text" class="form-control user-select-none" placeholder="TODO" readonly>
 
                         <div class="input-group-append">
                             <button type="button" class="btn btn-primary"><i class="bi bi-music-note-list"></i></button>
                         </div>
                     </div>
                 </form>
-                <button type="button" class="btn btn-primary"><i class="bi bi-arrow-right-square-fill"></i></button>
+                {{ next }}
             </nav>
-            HTML;
-
+            HTML,
+            previous: $this->previous,
+            next: $this->next,
+        );
     }
+
+    private Component\Button $previous;
+    private Component\Button $next;
 }
