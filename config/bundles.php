7d910861584f0d52939af614593628de93f412f4
Benoit VIGUIER <benoit.viguier.pro+github@gmail.com>
2025-03-28 22:15:37 +0100

    POC Twig Component

--- config/bundles.php
+++ config/bundles.php
@@ -5,5 +5,6 @@ declare(strict_types=1);
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'ipad' => true],
 ];
---
+++ src/Ui/Components/Alert.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bveing\MBuddy\Ui\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent]
+class Alert
+{
+
+}
